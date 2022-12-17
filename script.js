88c49760070410e700365407b600cee834535659
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-12-17 17:40:26 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-function findSingle(num, ar_size) {
-  // Iterate over every element
-  for (let i = 0; i < ar_size; i++) {
-    // Initialize count to 0
+function findSingle(num, arr_size) {
+  for (let i = 0; i < arr_size; i++) {
     let count = 0;
 
-    for (let j = 0; j < ar_size; j++) {
-      // Count the frequency
-      // of the element
+    for (let j = 0; j < arr_size; j++) {
       if (num[i] == num[j]) {
         count++;
       }
     }
-
-    // if the frequency of the
-    // element is one
     if (count == 1) {
       return num[i];
     }
   }
 
-  // if no element exist at most once
   return -1;
 }
 
-let ar = [2, 3, 5, 4, 5, 3, 4];
-let n = 7;
+let arr = [2, 3, 5, 4, 5, 3, 4];
+let totalElements = 7;
 
-// Function call
-console.log('Element occurring once is ' + findSingle(ar, n));
+console.log('Single Element is ' + findSingle(arr, totalElements));
